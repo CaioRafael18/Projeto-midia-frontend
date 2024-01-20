51b7f804a38d5cda328ad90ed4306eecb1c58ca8
Caio Rafael <caiorafaelbelarmino1@gmail.com>
2024-01-20 15:56:46 -0300

    Adicionando aviso para excluir

--- src/pages/Components/ListaLinks/index.js
+++ src/pages/Components/ListaLinks/index.js
@@ -1,22 +1,43 @@
 import React, { useState } from 'react'
 import iconeDelete from '../../image/icone-lixeira.png'
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 const ListaLinks = ({data, handleDelete}) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
     <li className="box_item">
       <div className='box_descricao'>
-        <a  href={data.descricao} target="_blank">
+        <a  href={data.descricao}>
           <strong>{data.title}</strong> 
         </a>
         
         <img 
           className='iconeDelete'
           src={iconeDelete} 
-          onClick={() => handleDelete(data._id)}
+          onClick={() => handleShow()}
         >
         </img>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Atenção!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Tem certeza que deseja excluir?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Não
+          </Button>
+          <Button variant="primary" onClick={() => handleDelete(data._id)}>
+            Sim
+          </Button>
+        </Modal.Footer>
+      </Modal>
 
       <div className='box_tag'>
         <span className='categoria'>{data.mes}</span>  
